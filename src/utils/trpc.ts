abd560159077f23e9f9fa6f47cc9c7c0150cc72f
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2023-04-20 14:34:03 +0700

    fix typo

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,7 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "@/server/routers/_app";
-import superjson from "superjson";"
+import superjson from "superjson";
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
@@ -22,7 +22,7 @@ export const trpc = createTRPCNext<AppRouter>({
           },
         }),
       ],
-      transformer: superjson
+      transformer: superjson,
     };
   },
   ssr: false,
